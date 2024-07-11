dd4176a634c822718df42502fdc245b84ec03733
lwfe <leonardolwfe@gmail.com>
2024-07-11 00:51:06 -0300

    feat: add some shadcn components

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function generateRandomHexCode(length = 32) {
+  const characters = "0123456789abcdef";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+}
