ff3bbd6f3e60c5e6c988fe982a6e5ed766d1db3f
lwfe <leonardolwfe@gmail.com>
2024-07-11 14:24:08 -0300

    fix: expiration date component hydration error

--- src/components/expiration-date/index.tsx
+++ src/components/expiration-date/index.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export function ExpirationDate() {
-  const date = new Date().toLocaleDateString();
-  const hourPlus4 = new Date(
-    new Date().setHours(new Date().getHours() + 4)
-  ).toLocaleTimeString();
+  const [date, setDate] = useState("");
+  const [hourPlus4, setHourPlus4] = useState("");
+
+  useEffect(() => {
+    const now = new Date();
+    const currentDate = now.toLocaleDateString();
+    const currentTimePlus4 = new Date(
+      now.setHours(now.getHours() + 4)
+    ).toLocaleTimeString();
+
+    setDate(currentDate);
+    setHourPlus4(currentTimePlus4);
+  }, []);
 
   return (
     <div className="flex flex-col items-center font-semibold mt-5">
